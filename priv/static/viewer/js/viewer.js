59a76f70cf72bd3dfb7fae762bcee69335c6204a
Martin Vejmelka <vejmelkam@gmail.com>
2013-06-21 19:54:58 -0600

    modified viewer app to show textual timestamp information

--- priv/static/viewer/js/viewer.js
+++ priv/static/viewer/js/viewer.js
@@ -1,19 +1,29 @@
 
+var field_prefix = "http://localhost:8080/field/colorado_2km_op_wrf_48hr_run";
+
+(function(){
+    /* detect URL for field retrieval */
+    var doc_loc = document.location.href;
+    var viewer_ndx = doc_loc.indexOf("/viewer");
+    field_prefix = doc_loc.substring(0, viewer_ndx) + "/field/fire_danger_operational_v1";
+ }());
+
 
 /* initial display settings */
-var web_prefix = "http://localhost:8080/field/colorado_2km_op_wrf_48hr_run";
 var current_var = "T2";
 var current_index = 0;
 var tss = "";
 
 function update_image() {
-    $("#fig_main").attr("src", web_prefix + "/" + current_var + "_" + tss[current_index]);
+    $("#fig_main").attr("src", field_prefix + "/" + current_var + "_" + tss[current_index]);
+    dt = tss[current_index].split("_");
+    $("#fig_text").text(current_var + " on " + dt[0] + " at " + dt[1]);
 }
 
 /* a short-hand for $(document).ready( ... ) */
 $(function() {
 
-    $.get(web_prefix + "/current_valid_ts", function(data) { tss = data.split("|"); update_image(); });
+    $.get(field_prefix + "/current_valid_ts", function(data) { tss = data.split("|"); update_image(); });
 
     /* construct the variable selector buttons */
     $( "#var_radio" ).buttonset();
